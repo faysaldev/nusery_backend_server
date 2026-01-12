7f9db5b691f46015bc89ea2e30683d2e66bede2a
Faysal Mridha <fmridha166@gmail.com>
2026-01-12 17:35:24 +0600

    convert to pnpm site

---
+++ app.ts
@@ -0,0 +1,25 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+import httpStatus from "http-status";
+import router from "./app/modules/products/product.route";
+import CategoryRouter from "./app/modules/category/category.route";
+import OrderRouter from "./app/modules/order/order.route";
+
+const app: Application = express();
+//  parser
+app.use(express.json());
+app.use(cors());
+
+// application routes
+app.use("/api/products", router);
+app.use("/api/categories", CategoryRouter);
+app.use("/api/orders", OrderRouter);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(httpStatus.OK).json({
+    success: true,
+    message: "👌Server is Running!",
+  });
+});
+
+export default app;
